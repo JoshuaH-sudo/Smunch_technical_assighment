c4accb2065baed5a1b2f669fdd4b449525fe726a
Joshua Hoban <joshua.hoban@countersight.co>
2023-07-18 14:26:32 +0200

    add auth modal to login or register

---
+++ src/client/components/auth/Auth_modal.tsx
@@ -0,0 +1,93 @@
+import {
+  EuiButton,
+  EuiFieldText,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiForm,
+  EuiFormRow,
+  EuiModal,
+  EuiModalBody,
+  EuiModalFooter,
+  EuiModalHeader,
+  EuiRadio,
+} from "@elastic/eui";
+import React, { FC, useState } from "react";
+import { capitalize } from "../utils/tools";
+import { create } from "../utils/api";
+
+interface Auth_modal_props {
+  close_modal: () => void;
+}
+
+const Auth_modal: FC<Auth_modal_props> = ({ close_modal }) => {
+  const [mode, set_mode] = useState<"login" | "register">("login");
+  const [username, set_username] = useState("");
+  const [password, set_password] = useState("");
+
+  const on_submit = async () => {
+    let user_id: string;
+    if (mode === "login") {
+      const response = await create<string>("auth/login");
+      user_id = response.data;
+    } else {
+      const response = await create<string>("auth/register");
+      user_id = response.data;
+    }
+
+    if (!user_id) throw "User id was not returned";
+
+    localStorage.setItem("user_id", user_id);
+  };
+
+  const mode_select = (
+    <EuiFlexGroup>
+      <EuiFlexItem>
+        <EuiRadio
+          id="login_radio"
+          label="Login"
+          checked={mode === "login"}
+          onChange={() => set_mode("login")}
+        />
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiRadio
+          id="register_radio"
+          label="Register"
+          checked={mode === "register"}
+          onChange={() => set_mode("register")}
+        />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+
+  return (
+    <EuiModal onClose={close_modal}>
+      <EuiModalHeader>{capitalize(mode)}</EuiModalHeader>
+
+      <EuiModalBody>
+        <EuiForm>
+          <EuiFormRow label="username">
+            <EuiFieldText
+              value={username}
+              onChange={(event) => set_username(event.target.value)}
+            />
+          </EuiFormRow>
+          <EuiFormRow label="password">
+            <EuiFieldText
+              type="password"
+              value={password}
+              onChange={(event) => set_password(event.target.value)}
+            />
+          </EuiFormRow>
+          <EuiFormRow>{mode_select}</EuiFormRow>
+        </EuiForm>
+      </EuiModalBody>
+
+      <EuiModalFooter>
+        <EuiButton onClick={on_submit}>Submit</EuiButton>
+      </EuiModalFooter>
+    </EuiModal>
+  );
+};
+
+export default Auth_modal;
--- src/client/components/review/Add_review_modal.tsx
+++ src/client/components/review/Add_review_modal.tsx
@@ -12,9 +12,10 @@ import {
   EuiModalHeader,
   EuiTextArea,
 } from "@elastic/eui";
-import React, { FC, ReactNode, useState } from "react";
+import React, { FC, ReactNode, useEffect, useState } from "react";
 import { New_review, Review_info } from "../../../server/models/review";
 import Select_rating from "./Select_rating";
+import Auth_modal from "../auth/Auth_modal";
 
 interface Add_review_modal_props {
   review_item_type: "Restaurant" | "Product";
@@ -33,11 +34,22 @@ const Add_review_modal: FC<Add_review_modal_props> = ({
     rating: 1,
     title: "",
     comment_text: "",
+    user_id: "",
   });
 
+  const [show_auth, set_show_auth] = useState(false);
+
+  useEffect(() => {
+    const user_id = localStorage.getItem("user_id");
+    if (user_id) set_show_auth(true);
+  }, [show_auth]);
+
   const on_confirm_handler = () => {
+    const user_id = localStorage.getItem("user_id");
+    if (!user_id) throw "User id not provided";
+
     if (review_value) {
-      on_confirm(review_value);
+      on_confirm({ ...review_value, user_id });
     }
   };
 
@@ -51,65 +63,69 @@ const Add_review_modal: FC<Add_review_modal_props> = ({
   const confirm_is_disabled = title.length === 0 || comment_text.length === 0;
 
   return (
-    <EuiModal onClose={close_modal}>
-      <EuiModalHeader>{`Add ${review_item_type} Review`}</EuiModalHeader>
+    <>
+      <EuiModal onClose={close_modal}>
+        <EuiModalHeader>{`Add ${review_item_type} Review`}</EuiModalHeader>
 
-      <EuiModalBody>
-        <EuiFlexGroup direction="column">
-          <EuiFlexItem grow={false}>{item_display}</EuiFlexItem>
+        <EuiModalBody>
+          <EuiFlexGroup direction="column">
+            <EuiFlexItem grow={false}>{item_display}</EuiFlexItem>
 
-          <EuiFlexItem grow={false}>
-            <EuiHorizontalRule />
-          </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <EuiHorizontalRule />
+            </EuiFlexItem>
 
-          <EuiFlexItem>
-            <EuiForm>
-              <EuiFormRow label="Rating">
-                <Select_rating
-                  rating={rating}
-                  on_rating_change={(rating) => update_form({ rating })}
-                />
-              </EuiFormRow>
+            <EuiFlexItem>
+              <EuiForm>
+                <EuiFormRow label="Rating">
+                  <Select_rating
+                    rating={rating}
+                    on_rating_change={(rating) => update_form({ rating })}
+                  />
+                </EuiFormRow>
 
-              <EuiFormRow label="Title">
-                <EuiFieldText
-                  value={title}
-                  onChange={(event) =>
-                    update_form({ title: event.target.value })
-                  }
-                />
-              </EuiFormRow>
+                <EuiFormRow label="Title">
+                  <EuiFieldText
+                    value={title}
+                    onChange={(event) =>
+                      update_form({ title: event.target.value })
+                    }
+                  />
+                </EuiFormRow>
 
-              <EuiFormRow label="Comment">
-                <EuiTextArea
-                  value={comment_text}
-                  onChange={(event) =>
-                    update_form({ comment_text: event.target.value })
-                  }
-                />
-              </EuiFormRow>
-            </EuiForm>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiModalBody>
+                <EuiFormRow label="Comment">
+                  <EuiTextArea
+                    value={comment_text}
+                    onChange={(event) =>
+                      update_form({ comment_text: event.target.value })
+                    }
+                  />
+                </EuiFormRow>
+              </EuiForm>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiModalBody>
 
-      <EuiModalFooter>
-        <EuiFlexGroup justifyContent="spaceBetween">
-          <EuiFlexItem>
-            <EuiButton onClick={close_modal}>Cancel</EuiButton>
-          </EuiFlexItem>
+        <EuiModalFooter>
+          <EuiFlexGroup justifyContent="spaceBetween">
+            <EuiFlexItem>
+              <EuiButton onClick={close_modal}>Cancel</EuiButton>
+            </EuiFlexItem>
 
-          <EuiFlexItem>
-            <EuiButton
-              isDisabled={confirm_is_disabled}
-              onClick={on_confirm_handler}
-            >
-              Confirm
-            </EuiButton>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiModalFooter>
-    </EuiModal>
+            <EuiFlexItem>
+              <EuiButton
+                isDisabled={confirm_is_disabled}
+                onClick={on_confirm_handler}
+              >
+                Confirm
+              </EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiModalFooter>
+      </EuiModal>
+
+      {show_auth && <Auth_modal close_modal={() => set_show_auth(false)} />}
+    </>
   );
 };
 
